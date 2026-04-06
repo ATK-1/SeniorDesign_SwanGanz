c5a64eb84ec7c4edbf6a9ac130d0d205f22e081d
Nathan <nathan.delarosa89@gmail.com>
2026-04-05 22:10:36 -0500

    port connection and disconnection

--- SwanGanzApp/src-tauri/src/lib.rs
+++ SwanGanzApp/src-tauri/src/lib.rs
@@ -1,10 +1,7 @@
 use tauri::{AppHandle, Emitter, State};
-use tauri_plugin_serialplugin::commands::{available_ports, close, open, read, write};
-
-#[tauri::command]
-fn ping() -> String {
-    "pong".to_string()
-}
+use tauri_plugin_serialplugin::commands::{
+    available_ports, close, managed_ports, open, read, write,
+};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -12,11 +9,15 @@ pub fn run() {
         .plugin(tauri_plugin_serialplugin::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_log::Builder::new().build())
-        .invoke_handler(tauri::generate_handler![check_connected, ping])
+        .invoke_handler(tauri::generate_handler![
+            check_connected,
+            check_disconnected
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+// Checks available ports and opens port if found. Emits "port-connected"
 #[tauri::command]
 async fn check_connected(
     app: AppHandle<tauri::Wry>,
@@ -26,7 +27,43 @@ async fn check_connected(
 
     for (port_name, _port_info) in ports.iter() {
         if port_name.contains("cu.usbserial-A106DAXQ") {
+            open(
+                app.clone(),
+                serial.clone(),
+                port_name.to_string(),
+                9600,
+                None,
+                None,
+                None,
+                None,
+                None,
+            );
             app.emit("port-connected", port_name).unwrap();
+        }
+    }
+
+    Ok(())
+}
+
+// Checks managed ports and emits "port-disconnected" if empty
+#[tauri::command]
+async fn check_disconnected(
+    app: AppHandle<tauri::Wry>,
+    serial: State<'_, tauri_plugin_serialplugin::desktop_api::SerialPort<tauri::Wry>>,
+) -> Result<(), String> {
+    let open_ports = managed_ports(app.clone(), serial.clone()).map_err(|e| e.to_string())?;
+    for port_name in open_ports.iter() {
+        if port_name.contains("cu.usbserial-A106DAXQ") {
+            let result = read(
+                app.clone(),
+                serial.clone(),
+                port_name.to_string(),
+                Some(1),
+                Some(100),
+            );
+            if result.is_err() {
+                app.emit("port-disconnected", port_name).unwrap();
+            }
         }
     }
 
--- SwanGanzApp/src/main.js
+++ SwanGanzApp/src/main.js
@@ -2,23 +2,40 @@ import { listen } from "@tauri-apps/api/event";
 import { invoke } from '@tauri-apps/api/core';
 
 let pollInterval;
-
-try {
-    invoke("ping")
-} catch (e) {
-    console.error("ping error:", e);
-}
+let connected = false;
 
 await listen("port-connected", (event) => {
-    console.log("Port found: ", event.payload);
-    clearInterval(pollInterval);
-    // transition UI state here
+    const element = document.getElementsByClassName("connection-status")[0];
+    element.textContent = "Connected";
+    console.log("Connected");
+    connected = true;
 });
 
+await listen("port-disconnected", (event) => {
+    const element = document.getElementsByClassName("connection-status")[0];
+    element.textContent = "Not Connected";
+    console.log("Disconnected");
+    connected = false;
+});
+
+
+
 pollInterval = setInterval(async () => {
-    try {
-        await invoke("check_connected");
-    } catch (e) {
-        console.error("Error checking ports:", e);
+    if (!connected) {
+        try {
+            await invoke("check_connected");
+        }
+        catch (e) {
+            console.error("Error checking ports:", e);
+        }
+    }
+    else {
+        try {
+            await invoke("check_disconnected");
+        }
+        catch (e) {
+            console.error("Error checking ports:", e);
+        }
     }
 }, 1500);
+
